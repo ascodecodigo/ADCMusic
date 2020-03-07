ae5fd7d904f4752cbddc088f15c36a7262790cec
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-03-06 19:54:42 -0600

    Layout de la aplicacion

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
     <!-- Navbar -->
+    <Navbar/>
+
+    <!-- Dynamic routes based on url -->
     <router-view/>
+
      <!-- Footer -->
+     <Footer/>
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+    Navbar: () => import('@/layout/Navbar'),
+    Footer: () => import('@/layout/Footer')
+  }
+}
+</script>
---
+++ src/layout/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar is-right is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="https://imagesadc.s3.amazonaws.com/logos/logohor_bco2.png"
+          width="auto" height="auto">
+        <span class="nav-title"><strong>AscoMusic</strong></span>
+      </a>
+      <a class="navbar-burger burger is-active" role="button" aria-label="menu"
+        aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" id="navbarBasicExample">
+      <div class="navbar-start"></div>
+      <div class="navbar-end">
+        <a class="navbar-item">Home</a>
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-danger">Logout</a>
+            <a class="button is-light">Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
