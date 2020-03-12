28b217e1c5427a22f332e6c1e77e53d856da17bc
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-03-11 21:54:22 -0600

    emplesar mas de un servicio

---
+++ src/services/auth.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: 'https://fake-auth-api.herokuapp.com',
+  mode: 'cors',
+  credetials: 'same-origin',
+  timeout: 20000
+})
+
+// // interceptar la petición
+// service.interceptors.request.use(config => {
+
+//   // modoficar las cabeceras
+//   if (!config.upload) config.headers.post['Content-Type'] = 'application/json'
+
+//   // modificar cabeceras
+//   config.headers.Authorization = 'Bearer token'
+//   return config
+// })
+
+// interceptar la respuesta
+service.interceptors.response.use(res => {
+  // errores del servidor
+  // validar codigos de estado
+  // modificar la respuesta
+  return res.data
+})
+
+export default service
--- src/services/axios.js
+++ src/services/axios.js
@@ -1,10 +1,29 @@
 import axios from 'axios'
 
 const service = axios.create({
-    baseURL: 'https://adc-spotify-api.herokuapp.com',
-    mode: 'cors',
-    credetials: 'same-origin',
-    timeout: 20000
+  baseURL: 'https://adc-spotify-api.herokuapp.com',
+  mode: 'cors',
+  credetials: 'same-origin',
+  timeout: 20000
+})
+
+// interceptar la petición
+service.interceptors.request.use(config => {
+
+  // modoficar las cabeceras
+  if (!config.upload) config.headers.post['Content-Type'] = 'application/json'
+
+  // modificar cabeceras
+  config.headers.Authorization = 'Bearer token'
+  return config
+})
+
+// interceptar la respuesta
+service.interceptors.response.use(res => {
+  // errores del servidor
+  // validar codigos de estado
+  // modificar la respuesta
+  return res.data
 })
 
 export default service
